24d26806d59f4849dd2a1613da4497706bfcf0d3
Stavros Kontopoulos <stavros.kontopoulos@lightbend.com>
2019-02-01 13:11:52 +0000

    minor modifications for fdp 2.0.1

--- resource-managers/kubernetes/lightbend-build/history-server/entrypoint.sh
+++ resource-managers/kubernetes/lightbend-build/history-server/entrypoint.sh
@@ -1,27 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # echo commands to the terminal output
 set -ex
 
+enablePVC=$1
+enableGCS=$2
+eventsDir=$3
+gcloudKey=$4
+
 # Check whether there is a passwd entry for the container UID
-myuid=$(id -u)
-mygid=$(id -g)
+uid=$(id -u)
+gid=$(id -g)
 # turn off -e for getent because it will return error code in anonymous uid case
 set +e
-uidentry=$(getent passwd $myuid)
+uid_entry=$(getent passwd ${uid})
 set -e
 
-echo $myuid
-echo $mygid
-echo $uidentry
-
 # If there is no passwd entry for the container UID, attempt to create one
-if [ -z "$uidentry" ] ; then
-    if [ -w /etc/passwd ] ; then
-        echo "$myuid:x:$myuid:$mygid:anonymous uid:$SPARK_HOME:/bin/false" >> /etc/passwd
+if [[ -z "${uid_entry}" ]] ; then
+    if [[ -w /etc/passwd ]] ; then
+        echo "$uid:x:$uid:$gid:anonymous uid:${SPARK_HOME}:/bin/false" >> /etc/passwd
     else
-        echo "Container ENTRYPOINT failed to add passwd entry for anonymous UID"
+        echo "Container entrypoint.sh failed to add passwd entry for anonymous UID"
     fi
 fi
 
-exec /sbin/tini -s -- /opt/spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer "$@"
+if [[ "$enablePVC" == "true" ]]; then
+    export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS \
+    -Dspark.history.fs.logDirectory=file:/mnt/$eventsDir";
+elif [[ "$enableGCS" == "true" ]]; then
+    if [[ -n ${gcloudKey} ]]; then
+      export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS \
+      -Dspark.hadoop.google.cloud.auth.service.account.json.keyfile=/etc/secrets/$gcloudKey \
+      -Dspark.history.fs.logDirectory=$eventsDir";
+    else
+      echo "Please pass your Google Cloud Key!"
+      exit 1
+    fi
+else
+    export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS \
+    -Dspark.history.fs.logDirectory=$eventsDir";
+fi;
+
+exec /sbin/tini -s -- /opt/spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer
+
